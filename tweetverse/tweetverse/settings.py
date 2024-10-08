5b06b59c46346179fe6c415d73563ae387586879
lobuchi <108718648+lobuchi@users.noreply.github.com>
2024-10-08 18:37:34 +0000

    up

--- tweetverse/tweetverse/settings.py
+++ tweetverse/tweetverse/settings.py
@@ -57,9 +57,9 @@ DATABASES = {
 }
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
